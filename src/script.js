2df1e8f474fcecb81313f849fb24a4ef286b0de4
Simon <SimonDutkaWork@gmail.com>
2022-02-02 11:54:44 +0100

    Added: Counting words

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
 const textArea = document.getElementById('text-area');
-const wordsValue = document.getElementById('words-value');
 const charactersValue = document.getElementById('characters-value');
+const wordsValue = document.getElementById('words-value');
 
-textArea.addEventListener('keyup', count);
+let numOfWords = 0,
+    numOfCharacters = 0;
 
-let numOfCharacters = 0;
-
-function count() {
+textArea.addEventListener('keyup', () => {
     let textAreaValue = textArea.value.split('str');
+
+    numOfWords = textArea.value.trim().split(/\s+/).length;
 
     for (let i = 0; i < textAreaValue.length; i++) {
         numOfCharacters = textAreaValue[i].length;
     }
 
+    const defaultValues = numOfCharacters === 0 ? (numOfWords = 0) : 0;
+
+    wordsValue.innerHTML = numOfWords;
     charactersValue.innerHTML = numOfCharacters;
-}
+});
